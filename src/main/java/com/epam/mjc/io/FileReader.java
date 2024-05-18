dd85060d3f5c56024eb7d79945b7f092020a46c7
Nikita <hrynts2002@gmail.com>
2024-05-18 03:35:04 +0300

    Completed task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
+    Profile profile = new Profile();
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                parseString(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return profile;
+    }
+
+    public void parseString(String line) {
+        String[] elements = line.split(" ");
+        switch (elements[0]) {
+            case "Name:":
+                profile.setName(elements[1]);
+                break;
+            case "Age:":
+                profile.setAge(Integer.valueOf(elements[1]));
+                break;
+            case "Email:":
+                profile.setEmail(elements[1]);
+                break;
+            case "Phone:":
+                profile.setPhone(Long.valueOf(elements[1]));
+                break;
+        }
     }
 }
